1a1aba152a0cff862edf53756bfcae613d4b0a1e
8-ball43 <mateuszzawisza13@gmail.com>
2025-11-04 10:45:50 +0100

    Add files via upload

---
+++ arosaka.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Lis 04, 2025 at 10:42 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arosaka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uzytkownicy`
+--
+
+CREATE TABLE `uzytkownicy` (
+  `id_uzytkownika` int(11) NOT NULL,
+  `login` varchar(25) DEFAULT NULL,
+  `haslo` varchar(25) DEFAULT NULL,
+  `adres` varchar(50) DEFAULT NULL,
+  `aktualne_srodki` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wplaty`
+--
+
+CREATE TABLE `wplaty` (
+  `id_wplaty` int(11) NOT NULL,
+  `id_uzytkownika` int(11) DEFAULT NULL,
+  `kwota` float DEFAULT NULL,
+  `opis` text DEFAULT NULL,
+  `data` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`id_uzytkownika`),
+  ADD UNIQUE KEY `login` (`login`),
+  ADD UNIQUE KEY `adres` (`adres`);
+
+--
+-- Indeksy dla tabeli `wplaty`
+--
+ALTER TABLE `wplaty`
+  ADD PRIMARY KEY (`id_wplaty`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `id_uzytkownika` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wplaty`
+--
+ALTER TABLE `wplaty`
+  MODIFY `id_wplaty` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
