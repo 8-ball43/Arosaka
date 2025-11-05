fe944d541ae978771eaff8d0e10ce755773b29cb
8-ball43 <mateuszzawisza13@gmail.com>
2025-11-05 09:34:40 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -20,35 +20,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <h1>Strona</h1>
-        <nav>
-        <ul>
-            <li><a href="">Test</a></li>
-            <li><a href="">Test</a></li>
-            <li><a href="">Test</a></li>
-            <li><a href="">Test</a></li>
-        </ul>
-    </nav>    
-    </header>
-    <div class="container">
-        <form method="post">
-        <h2 style="text-align:center">Login</h2>
-        <div class="col">
-            <input type="text" name="login" placeholder="Login" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" value="Login">
-        </div>
-        </form>
-    </div>
-</body>
-</html>
+
